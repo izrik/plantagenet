d25c90094d9f003333cd4d4e04ec8d17d8524077
izrik <izrik@izrik.com>
2018-06-15 14:50:13 -0500

    Fix the shell script.

--- run_tests_with_coverage.sh
+++ run_tests_with_coverage.sh
@@ -2,6 +2,6 @@
 
 coverage run --source=plantagenet ./run_tests.py "$@" && \
     coverage html && \
-    flake8 plantagenet.py run_tests.py
-    shellcheck run_tests_with_coverage.sh
+    flake8 plantagenet.py run_tests.py && \
+    shellcheck run_tests_with_coverage.sh && \
     markdownlint README.md
