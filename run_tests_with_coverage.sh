d7274019ef3a1e055c9effbbb9d5671536c751d0
izrik <izrik@izrik.com>
2018-06-15 14:50:41 -0500

    Lint the css file.

--- run_tests_with_coverage.sh
+++ run_tests_with_coverage.sh
@@ -4,4 +4,5 @@ coverage run --source=plantagenet ./run_tests.py "$@" && \
     coverage html && \
     flake8 plantagenet.py run_tests.py && \
     shellcheck run_tests_with_coverage.sh && \
-    markdownlint README.md
+    markdownlint README.md && \
+    csslint static/plantagenet.css
